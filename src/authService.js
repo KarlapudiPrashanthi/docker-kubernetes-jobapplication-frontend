386957a84879b7e726e341fddf30d39e08d8ec6f
KarlapudiPrashanthi <prashanthikarlapudi7@gmail.com>
2025-11-28 07:25:12 +0530

    Update API_URL to point to local server

--- src/authService.js
+++ src/authService.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const API_URL = "http://44.220.64.228:8080/auth";
+const API_URL = "http://localhost:30082/back1/auth";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
